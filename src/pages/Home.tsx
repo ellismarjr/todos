a6cb4cb69f2e0a7f98082192a95e8ecb07782dec
Junior de Oliveira <ellismarjr@hotmail.com>
2021-05-21 10:17:46 -0300

    challenge finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,34 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (newTaskTitle) {
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      };
+
+      setTasks(prevArray => [...prevArray, data]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const taskExists = tasks.find(task => task.id === id);
+    if (taskExists) {
+      const tasksFiltered = tasks.map(task => task.id === id
+        ? { ...task, done: !task.done }
+        : task);
+
+      setTasks(tasksFiltered);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksFiltered = tasks.filter(task => task.id !== id);
+    setTasks(tasksFiltered);
   }
 
   return (
@@ -31,10 +47,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
   )
